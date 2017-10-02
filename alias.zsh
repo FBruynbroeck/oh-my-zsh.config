1f155f20ddfa133050980a1c08ef9ad5c3c10e88
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2017-10-02 13:55:42 +0200

    Remove psql alias

--- alias.zsh
+++ alias.zsh
@@ -25,9 +25,6 @@ alias rmswp='find . -name "*.swp" -exec rm -rf {} \;'
 alias copy-version="echo \`cat setup.py | grep 'name=' | cut -d \' -f2; cat setup.py | grep dev | awk '{print \$3}' | sed -r \"s/^'//;s/.{6}$//\"\` | pbcopy"
 # Thefuck
 alias fuck='eval $(thefuck $(fc -ln -1 | tail -n 1)); fc -R'
-# PostgresSQL cerise
-alias psql-cerise-start="cd ${buildout}/ARSIA/docker-cerise-postgres && make up && cd -"
-alias psql-cerise-stop="cd ${buildout}/ARSIA/docker-cerise-postgres && make stop && cd -"
 # Git
 alias gh="git remote -v | awk '/fetch/{print \$2}' | sed -Ee 's#(git@|git://)#http://#' -e 's@org:@org/@' -e 's@com:@com/@' -e 's@be:@be/@' -e 's@\.git\$@@'"
 # Vim
